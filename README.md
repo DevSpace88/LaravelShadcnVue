8a1aa109b3e33e110c15da796be83dfd0a085b9e
DevSpace88 <sgorochow@hotmail.de>
2024-09-09 02:35:49 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,66 +1,52 @@
-<p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400" alt="Laravel Logo"></a></p>
-
-<p align="center">
-<a href="https://github.com/laravel/framework/actions"><img src="https://github.com/laravel/framework/workflows/tests/badge.svg" alt="Build Status"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/dt/laravel/framework" alt="Total Downloads"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/v/laravel/framework" alt="Latest Stable Version"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/l/laravel/framework" alt="License"></a>
-</p>
-
-## About Laravel
-
-Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel takes the pain out of development by easing common tasks used in many web projects, such as:
-
-- [Simple, fast routing engine](https://laravel.com/docs/routing).
-- [Powerful dependency injection container](https://laravel.com/docs/container).
-- Multiple back-ends for [session](https://laravel.com/docs/session) and [cache](https://laravel.com/docs/cache) storage.
-- Expressive, intuitive [database ORM](https://laravel.com/docs/eloquent).
-- Database agnostic [schema migrations](https://laravel.com/docs/migrations).
-- [Robust background job processing](https://laravel.com/docs/queues).
-- [Real-time event broadcasting](https://laravel.com/docs/broadcasting).
-
-Laravel is accessible, powerful, and provides tools required for large, robust applications.
-
-## Learning Laravel
-
-Laravel has the most extensive and thorough [documentation](https://laravel.com/docs) and video tutorial library of all modern web application frameworks, making it a breeze to get started with the framework.
-
-You may also try the [Laravel Bootcamp](https://bootcamp.laravel.com), where you will be guided through building a modern Laravel application from scratch.
-
-If you don't feel like reading, [Laracasts](https://laracasts.com) can help. Laracasts contains thousands of video tutorials on a range of topics including Laravel, modern PHP, unit testing, and JavaScript. Boost your skills by digging into our comprehensive video library.
-
-## Laravel Sponsors
-
-We would like to extend our thanks to the following sponsors for funding Laravel development. If you are interested in becoming a sponsor, please visit the [Laravel Partners program](https://partners.laravel.com).
-
-### Premium Partners
-
-- **[Vehikl](https://vehikl.com/)**
-- **[Tighten Co.](https://tighten.co)**
-- **[WebReinvent](https://webreinvent.com/)**
-- **[Kirschbaum Development Group](https://kirschbaumdevelopment.com)**
-- **[64 Robots](https://64robots.com)**
-- **[Curotec](https://www.curotec.com/services/technologies/laravel/)**
-- **[Cyber-Duck](https://cyber-duck.co.uk)**
-- **[DevSquad](https://devsquad.com/hire-laravel-developers)**
-- **[Jump24](https://jump24.co.uk)**
-- **[Redberry](https://redberry.international/laravel/)**
-- **[Active Logic](https://activelogic.com)**
-- **[byte5](https://byte5.de)**
-- **[OP.GG](https://op.gg)**
-
-## Contributing
-
-Thank you for considering contributing to the Laravel framework! The contribution guide can be found in the [Laravel documentation](https://laravel.com/docs/contributions).
-
-## Code of Conduct
-
-In order to ensure that the Laravel community is welcoming to all, please review and abide by the [Code of Conduct](https://laravel.com/docs/contributions#code-of-conduct).
-
-## Security Vulnerabilities
-
-If you discover a security vulnerability within Laravel, please send an e-mail to Taylor Otwell via [taylor@laravel.com](mailto:taylor@laravel.com). All security vulnerabilities will be promptly addressed.
-
-## License
-
-The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+# Starter Laravel Inertia Vue ShadCn Tailwind IBM Plex Font
+Verwendet diese UI-Library:
+(shadcn-vue)[https://www.shadcn-vue.com/]
+# Folgende Komponenten sind bereits installiert:
+npx shadcn-vue@latest add button
+npx shadcn-vue@latest add dropdown-menu
+npx shadcn-vue@latest add card
+npx shadcn-vue@latest add input
+npx shadcn-vue@latest add label
+npx shadcn-vue@latest add select
+npx shadcn-vue@latest add alert
+npx shadcn-vue@latest add alert-dialog
+npx shadcn-vue@latest add avatar
+npx shadcn-vue@latest add aspect-ratio
+npx shadcn-vue@latest add badge
+npx shadcn-vue@latest add breadcrumb
+npx shadcn-vue@latest add calendar
+npx shadcn-vue@latest add carousel
+npx shadcn-vue@latest add checkbox
+npx shadcn-vue@latest add command
+npx shadcn-vue@latest add collapsible
+npx shadcn-vue@latest add context-menu
+npx shadcn-vue@latest add table
+npx shadcn-vue@latest add dialog
+npx shadcn-vue@latest add drawer
+npx shadcn-vue@latest add form
+npx shadcn-vue@latest add hover-card
+npx shadcn-vue@latest add menubar
+npx shadcn-vue@latest add navigation-menu
+npx shadcn-vue@latest add number-field
+npx shadcn-vue@latest add pagination
+npx shadcn-vue@latest add pin-input
+npx shadcn-vue@latest add popover
+npx shadcn-vue@latest add progress
+npx shadcn-vue@latest add radio-group
+npx shadcn-vue@latest add range-calendar
+npx shadcn-vue@latest add resizable
+npx shadcn-vue@latest add scroll-area
+npx shadcn-vue@latest add separator
+npx shadcn-vue@latest add sheet
+npx shadcn-vue@latest add skeleton
+npx shadcn-vue@latest add slider
+npx shadcn-vue@latest add sonner
+npx shadcn-vue@latest add stepper
+npx shadcn-vue@latest add switch
+npx shadcn-vue@latest add tabs
+npx shadcn-vue@latest add tags-input
+npx shadcn-vue@latest add textarea
+npx shadcn-vue@latest add toast
+npx shadcn-vue@latest add toggle
+npx shadcn-vue@latest add toggle-group
+npx shadcn-vue@latest add tooltip
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,57 @@
 <script setup lang="ts">
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card'
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Button } from '@/components/ui/button'
+
+import { type Ref, ref } from 'vue'
+import {
+    CalendarDate,
+    DateFormatter,
+    getLocalTimeZone,
+} from '@internationalized/date'
+
+import { Calendar as CalendarIcon } from 'lucide-vue-next'
+import type { DateRange } from 'radix-vue'
+import { RangeCalendar } from '@/components/ui/range-calendar'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { cn } from '@/lib/utils'
+import {Toaster} from "vue-sonner";
+
+const df = new DateFormatter('en-US', {
+    dateStyle: 'medium',
+})
+
+const value = ref({
+    start: new CalendarDate(2022, 1, 20),
+    end: new CalendarDate(2022, 1, 20).add({ days: 20 }),
+}) as Ref<DateRange>
+
+const defaultValue = 'item-1'
+
+const accordionItems = [
+    { value: 'item-1', title: 'Is it accessible?', content: 'Yes. It adheres to the WAI-ARIA design pattern.' },
+    { value: 'item-2', title: 'Is it unstyled?', content: 'Yes. It\'s unstyled by default, giving you freedom over the look and feel.' },
+    { value: 'item-3', title: 'Can it be animated?', content: 'Yes! You can use the transition prop to configure the animation.' },
+]
+
 </script>
 
 <template>
@@ -8,15 +59,129 @@ import { Head } from '@inertiajs/vue3';
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>
+            <h2 class="font-semibold text-xl leading-tight">Dashboard</h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
+                <div class=" overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 font-bold"><h1>Keine Ahnung was ein Toaster macht</h1></div>
                 </div>
             </div>
         </div>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class=" overflow-hidden shadow-sm sm:rounded-lg">
+                    <p class="p-6">
+                        <Accordion type="single" class="w-full" collapsible :default-value="defaultValue">
+                            <AccordionItem v-for="item in accordionItems" :key="item.value" :value="item.value">
+                                <AccordionTrigger>{{ item.title }}</AccordionTrigger>
+                                <AccordionContent>
+                                    {{ item.content }}
+                                </AccordionContent>
+                            </AccordionItem>
+                        </Accordion>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="overflow-hidden shadow-sm sm:rounded-lg">
+                    <p class="p-6 text-gray-900">
+            <Card class="w-[350px]">
+                <CardHeader>
+                    <CardTitle>Create project</CardTitle>
+                    <CardDescription>Deploy your new project in one-click.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form>
+                        <div class="grid items-center w-full gap-4">
+                            <div class="flex flex-col space-y-1.5">
+                                <Label for="name">Name</Label>
+                                <Input id="name" placeholder="Name of your project" />
+                            </div>
+                            <div class="flex flex-col space-y-1.5">
+                                <Label for="framework">Framework</Label>
+                                <Select>
+                                    <SelectTrigger id="framework">
+                                        <SelectValue placeholder="Select" />
+                                    </SelectTrigger>
+                                    <SelectContent position="popper">
+                                        <SelectItem value="nuxt">
+                                            Nuxt
+                                        </SelectItem>
+                                        <SelectItem value="next">
+                                            Next.js
+                                        </SelectItem>
+                                        <SelectItem value="sveltekit">
+                                            SvelteKit
+                                        </SelectItem>
+                                        <SelectItem value="astro">
+                                            Astro
+                                        </SelectItem>
+                                    </SelectContent>
+                                </Select>
+                            </div>
+                        </div>
+                    </form>
+                </CardContent>
+                <CardFooter class="flex justify-between px-6 pb-6">
+                    <Button variant="outline">
+                        Cancel
+                    </Button>
+                    <Button>Deploy</Button>
+                </CardFooter>
+            </Card>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-sky-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6">
+
+
+
+                            <Popover>
+                                <PopoverTrigger as-child>
+                                    <Button
+                                        variant="outline"
+                                        :class="cn(
+          'w-[280px] justify-start text-left font-normal',
+          !value && 'text-muted-foreground',
+        )"
+                                    >
+                                        <CalendarIcon class="mr-2 h-4 w-4" />
+                                        <template v-if="value.start">
+                                            <template v-if="value.end">
+                                                {{ df.format(value.start.toDate(getLocalTimeZone())) }} - {{ df.format(value.end.toDate(getLocalTimeZone())) }}
+                                            </template>
+
+                                            <template v-else>
+                                                {{ df.format(value.start.toDate(getLocalTimeZone())) }}
+                                            </template>
+                                        </template>
+                                        <template v-else>
+                                            Pick a date
+                                        </template>
+                                    </Button>
+                                </PopoverTrigger>
+                                <PopoverContent class="w-auto p-0">
+                                    <RangeCalendar v-model="value" initial-focus :number-of-months="2" @update:start-value="(startDate) => value.start = startDate" />
+                                </PopoverContent>
+                            </Popover>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </AuthenticatedLayout>
 </template>
