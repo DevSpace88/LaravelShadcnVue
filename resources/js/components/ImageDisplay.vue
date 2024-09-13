69c3faad94b78e0da013bdf70e7ddcad61aaa82c
sergejgorochow <s.gorochow@netstack.de>
2024-09-13 10:31:31 +0200

    added ImageDisplay Component Description

--- resources/js/components/ImageDisplay.vue
+++ resources/js/components/ImageDisplay.vue
@@ -21,3 +21,46 @@ const handleImageError = () => {
     imageError.value = true
 }
 </script>
+
+
+<!-- Benutzung der Komponente in der Page (als Slides von mehreren Bildern):
+
+const props = defineProps<{
+    imagePaths?: string[]
+}>()
+
+
+<Carousel v-slot="{ canScrollNext }" class="relative w-full mx-auto max-w-5xl">
+        <CarouselContent>
+            <CarouselItem v-for="(imagePath, index) in imagePaths" :key="index">
+                <div class="p-1">
+                    <Card>
+                        <CardContent class="flex aspect-square items-center justify-center p-6">
+                            <ImageDisplay :image-path="imagePath" alt-text="Galeriebild"/>
+                        </CardContent>
+                    </Card>
+                </div>
+            </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious/>
+        <CarouselNext v-if="canScrollNext"/>
+</Carousel>
+
+Im Laravel-Controller (bei mehreren Bildern als Array):
+
+public function index()
+    {
+        $imageFiles = Storage::files('public/images');
+        $imagePaths = array_map(function ($file) {
+            return str_replace('public/', '', $file);
+        }, $imageFiles);
+
+        return Inertia::render('Dashboard', [
+            'imagePaths' => $imagePaths
+        ]);
+    }
+
+
+als Einzelbild:
+<ImageDisplay :image-path="imagePath" alt-text="Galeriebild"/>
+-->
