0737609375ff39aec65c1c5f2a7731183144016c
SergejGo <102557040+DevSpace88@users.noreply.github.com>
2024-10-05 20:28:25 +0200

    Update Login.vue
    Login Screen is centered now

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -29,64 +29,66 @@ const submit = () => {
 </script>
 
 <template>
-    <Card class="w-full max-w-sm mx-auto pb-8 mt-48">
-        <CardHeader>
-          <ApplicationLogo class="h-9 w-auto mx-auto"/>
-            <CardTitle class="text-2xl">
-                Login
-            </CardTitle>
-            <CardDescription>
-                Enter your email below to login to your account.
-            </CardDescription>
-        </CardHeader>
-        <form @submit.prevent="submit">
-            <CardContent class="grid gap-4">
-                <div class="grid gap-2">
-                    <Label for="email">Email</Label>
-                    <Input
-                        id="email"
-                        type="email"
-                        v-model="form.email"
-                        required
-                        autofocus
-                        autocomplete="username"
-                        placeholder="m@example.com"
-                    />
-                    <InputError class="mt-2" :message="form.errors.email" />
-                </div>
-                <div class="grid gap-2">
-                    <Label for="password">Password</Label>
-                    <Input
-                        id="password"
-                        type="password"
-                        v-model="form.password"
-                        required
-                        autocomplete="current-password"
-                        placeholder="******"
-                    />
-                    <InputError class="mt-2" :message="form.errors.password" />
-                </div>
-                <div class="block mt-4">
-                    <label class="flex items-center">
-                        <Checkbox name="remember" v-model:checked="form.remember" />
-                        <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">Remember me</span>
-                    </label>
-                </div>
-            </CardContent>
-            <CardFooter>
-                <Button type="submit" class="w-full" :disabled="form.processing">
-                    <span v-if="form.processing" class="loader"></span> <!-- Loader anzeigen wenn in Verarbeitung -->
-                    <span v-else>Sign in</span>
-                </Button>
-            </CardFooter>
-        </form>
-        <div v-if="canResetPassword" class="text-center text-sm mt-4">
-            <Link
-                :href="route('password.request')"
-                class="underline text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100"
-            >
-                Forgot your password?
-            </Link>
-        </div>
-    </Card>
+    <div class="flex min-h-screen items-center justify-center">
+        <Card class="w-full max-w-sm mx-auto pb-8 mt-48">
+            <CardHeader>
+              <ApplicationLogo class="h-9 w-auto mx-auto"/>
+                <CardTitle class="text-2xl">
+                    Login
+                </CardTitle>
+                <CardDescription>
+                    Enter your email below to login to your account.
+                </CardDescription>
+            </CardHeader>
+            <form @submit.prevent="submit">
+                <CardContent class="grid gap-4">
+                    <div class="grid gap-2">
+                        <Label for="email">Email</Label>
+                        <Input
+                            id="email"
+                            type="email"
+                            v-model="form.email"
+                            required
+                            autofocus
+                            autocomplete="username"
+                            placeholder="m@example.com"
+                        />
+                        <InputError class="mt-2" :message="form.errors.email" />
+                    </div>
+                    <div class="grid gap-2">
+                        <Label for="password">Password</Label>
+                        <Input
+                            id="password"
+                            type="password"
+                            v-model="form.password"
+                            required
+                            autocomplete="current-password"
+                            placeholder="******"
+                        />
+                        <InputError class="mt-2" :message="form.errors.password" />
+                    </div>
+                    <div class="block mt-4">
+                        <label class="flex items-center">
+                            <Checkbox name="remember" v-model:checked="form.remember" />
+                            <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">Remember me</span>
+                        </label>
+                    </div>
+                </CardContent>
+                <CardFooter>
+                    <Button type="submit" class="w-full" :disabled="form.processing">
+                        <span v-if="form.processing" class="loader"></span> <!-- Loader anzeigen wenn in Verarbeitung -->
+                        <span v-else>Sign in</span>
+                    </Button>
+                </CardFooter>
+            </form>
+            <div v-if="canResetPassword" class="text-center text-sm mt-4">
+                <Link
+                    :href="route('password.request')"
+                    class="underline text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100"
+                >
+                    Forgot your password?
+                </Link>
+            </div>
+        </Card>
+    </div>
 </template>
